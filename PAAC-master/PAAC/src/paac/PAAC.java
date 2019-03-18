54792d520d5b1491c990eb74601e2c1b576cc233
AlfonsoRoblesA <36724723+AlfonsoRoblesA@users.noreply.github.com>
2019-03-17 18:59:29 -0600

    agrego registrar proyecto y su controlador

--- PAAC-master/PAAC/src/paac/PAAC.java
+++ PAAC-master/PAAC/src/paac/PAAC.java
@@ -14,7 +14,7 @@ public class PAAC extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("RegistrarTesis.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("RegistrarProyecto.fxml"));
 
         Scene scene = new Scene(root);
 
